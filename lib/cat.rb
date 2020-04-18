9ea99097324537bf2a706505e99f6f8a79e51d68
kenkuts <kennethchoi19@gmail.com>
2020-04-18 22:23:29 +0300

    commit

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+  def initialize(name, owner=nil)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_cat(cat)
+    Cat.all.find { |cat| cat.name == cat } || Cat.new(cat)
+  end
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+  def initialize(name, owner=nil)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_dog(dog)
+    Dog.all.find { |dog| dog.name == dog } || Dog.new(dog)
+  end
+
 end
